781decb0fd0d384dc0301cae6096f89e639529c5
GadjievAlex <gadjievalex@inbox.ru>
2014-12-25 03:41:13 +0400

    25.12.2014

---
+++ Design Patterns C#/Structural Patterns/CompositePattern/Simple/CompositePattern/Component.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompositePattern
+{
+    class Component<T>:IComponent<T>
+    {
+        public Component(T name)
+        {
+            Name = name;
+        }
+        public void Add(IComponent<T> c)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IComponent<T> Remove(T s)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Display(int depth)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IComponent<T> Find(T s)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Name
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ Design Patterns C#/Structural Patterns/CompositePattern/Simple/CompositePattern/IComponent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompositePattern
+{
+    public interface IComponent<T>
+    {
+        void Add(IComponent<T> c);
+        IComponent<T> Remove(T s);
+        string Display(int depth);
+        IComponent<T> Find(T s);
+        T Name { get; set; }
+    }
+}
