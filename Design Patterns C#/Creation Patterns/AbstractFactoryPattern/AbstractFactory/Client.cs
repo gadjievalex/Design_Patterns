754a5016ef9c54632d91c48ed68d171e46436055
GadjievAlex <gadjievalex@inbox.ru>
2015-01-19 00:36:39 +0300

    2

---
+++ Design Patterns C#/Creation Patterns/AbstractFactoryPattern/AbstractFactory/Client.cs
@@ -0,0 +1,21 @@
+﻿using AbstractFactory.interfaces;
+using AbstractFactory.interfaces.products;
+using System;
+
+namespace AbstractFactory
+{
+    class Client<Brand> where Brand:IBrand,new()
+    {
+        public void ClientMain()
+        {
+            IFactory<Brand> factory = new Factory<Brand>();
+
+            IBags bag = factory.CreateBag();
+
+            IShoes shoes = factory.CreateShoes();
+
+            Console.WriteLine(" bag material is - > " + bag.Material);
+            Console.WriteLine(" Price is - > " + shoes.Price);
+        }
+    }
+}
---
+++ Design Patterns C#/Creation Patterns/AbstractFactoryPattern/AbstractFactory/interfaces/products/IBags.cs
@@ -0,0 +1,11 @@
+﻿
+namespace AbstractFactory
+{
+    interface IBags
+    {
+        string Material
+        {
+            get;
+        }
+    }
+}
