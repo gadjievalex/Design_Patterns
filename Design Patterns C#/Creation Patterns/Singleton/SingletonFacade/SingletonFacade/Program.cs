2ce7374140eb58daf28c6be8c89da022a9e61c1a
GadjievAlex <gadjievalex@inbox.ru>
2015-01-18 22:28:32 +0300

    singletoneGenericClass

--- Design Patterns C#/Creation Patterns/Singleton/SingletonFacade/SingletonFacade/Program.cs
+++ Design Patterns C#/Creation Patterns/Singleton/SingletonFacade/SingletonFacade/Program.cs
@@ -4,13 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SingletonLazyInstantiation
+namespace SingletonFacade
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.ReadLine();
         }
     }
 }
--- Design Patterns C#/Creation Patterns/Singleton/SingletonPatternGenericCode/SingletonPatternGenericCode/Program.cs
+++ Design Patterns C#/Creation Patterns/Singleton/SingletonPatternGenericCode/SingletonPatternGenericCode/Program.cs
@@ -4,13 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SingletonLazyInstantiation
+namespace SingletonPatternGenericCode
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.ReadLine();
         }
     }
 }
